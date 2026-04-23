2256cab7a53702d10517a58a754a1bd73bc02972
Sathwik Shetty <sathwikshetty2k5@gmail.com>
2026-04-23 20:45:49 +0000

    backend: Include the user, auth exception handlers and user routes to the fastapi app.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
 from app.config import settings
+from app.exceptions.auth import register_auth_exception_handlers
+from app.exceptions.common import register_common_exception_handlers
+from app.exceptions.sql_alchemy import register_sql_alchemy_exception_handlers
+from app.routers.user import router as user_router
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
+
+register_auth_exception_handlers(app)
+register_common_exception_handlers(app)
+register_sql_alchemy_exception_handlers(app)
+
+app.include_router(user_router)
 
 
 @app.get("/health")
