b947b3d08d238c75af98cfac9c961a9e18ae9fcb
Sathwik Shetty <sathwikshetty2k5@gmail.com>
2026-05-12 03:50:51 +0000

    backend/worker: Extend the interface to include startup and shutdown.

--- backend/app/interfaces/background_worker.py
+++ backend/app/interfaces/background_worker.py
@@ -3,7 +3,15 @@ from abc import ABC, abstractmethod
 
 class BackgroundWorkerInterface(ABC):
     @abstractmethod
-    async def process_resume_task(
+    async def startup(self) -> None:
+        pass
+
+    @abstractmethod
+    async def shutdown(self) -> None:
+        pass
+
+    @abstractmethod
+    async def process_resume_task(  # type: ignore[misc]
         file_bytes_b64: str, file_name: str, application_id: int
     ) -> None:
         pass
